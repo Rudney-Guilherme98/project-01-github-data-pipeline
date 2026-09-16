3d3d64ce257de30585463831c96a9c7792eba7f0
Rudney Macedo <rudney.guilherme98@gmail.com>
2026-09-16 00:16:31 -0300

    refatora busca de repositorio em funcao reutilizavel

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import requests
 import json
 from dotenv import load_dotenv
+def buscar_repositorio (owner, repo, headers):
+    url = f"https://api.github.com/repos/{owner}/{repo}"
+    response = requests.get(url, headers=headers)
+    return response.json()
 
 load_dotenv()
 token = os.getenv("GITHUB_TOKEN")
@@ -11,10 +15,7 @@ headers = {
 response = requests.get("https://api.github.com/user", headers=headers)
 print (response.status_code)
 print (response.json())
-repo_url = "https://api.github.com/repos/psf/requests"
-repo_reponse = requests.get(repo_url, headers=headers)
-
-repo_data = repo_reponse.json()
+repo_data = buscar_repositorio("psf", "requests", headers)
 
 print("Nome:", repo_data["full_name"])
 print("Estrelas:", repo_data["stargazers_count"])
